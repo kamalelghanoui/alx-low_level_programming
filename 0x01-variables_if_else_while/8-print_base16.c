5fb258002cbc8bb28def443448e56bf908cde1e5
kamal el ghanoui <ghkama59@gmail.com>
2023-02-17 17:47:45 -0800

    r

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+i = 48;
+while (i <= 57)
+{
+putchar(i);
+i++;
+}
+i = 65;
+while (i <= 70)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
