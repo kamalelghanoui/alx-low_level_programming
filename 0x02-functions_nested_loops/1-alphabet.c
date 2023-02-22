8e630d8de9f2ae6323e4202ae496dd97a854716c
kamal el ghanoui <ghkama59@gmail.com>
2023-02-22 13:27:51 -0800

    r

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+
+int i;
+i = 97;
+while (i <= 122)
+{
+putchar(i);
+i++;
+}
+putchar('\n');
+return (0);
+}
